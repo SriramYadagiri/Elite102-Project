04cb3c7c2119502ff2d4b1e3ba35500a496c1ca2
SriramY <73784283+UserName835@users.noreply.github.com>
2023-04-25 08:09:31 -0500

    Added comments

--- banking.py
+++ banking.py
@@ -1,43 +1,35 @@
+#Returns the user's balance
 def get_balance(user):
     return user['money']
 
-def change_password(user, connection):
-    new_password = input("\nWhat do you want to change your password to: ")
-    change = input(f'Are you sure you want to change your password to {new_password} (Y/N) ').lower()
+#Changes the user's password to a new given one
+def change_password(user, connection, new_password):
+    cursor = connection.cursor()
 
-    if change != "n":
-        cursor = connection.cursor()
+    changeUserPassword = (f'UPDATE users SET pin = \"{new_password}\" WHERE id = {user["id"]}')
 
-        changeUserPassword = (f'UPDATE users SET pin = \"{new_password}\" WHERE id = {user["id"]}')
+    cursor.execute(changeUserPassword)
 
-        cursor.execute(changeUserPassword)
+    connection.commit()
+    cursor.close()
 
-        connection.commit()
-        cursor.close()
+    user["pin"] = new_password
 
-        user["pin"] = new_password
+#Changes the user's name to a new given one
+def change_name(user, connection, new_name):
+    cursor = connection.cursor()
 
-def change_name(user, connection):
-    new_name = input("\nWhat do you want to change your name to: ")
-    change = input(f'Are you sure you want to change your name to \"{new_name}\" (Y/N) ').lower()
+    changeUserName = (f'UPDATE users SET name = \"{new_name}\" WHERE id = {user["id"]}')
 
-    if change != "n":
-        cursor = connection.cursor()
+    cursor.execute(changeUserName)
 
-        changeUserName = (f'UPDATE users SET name = \"{new_name}\" WHERE id = {user["id"]}')
+    connection.commit()
+    cursor.close()
 
-        cursor.execute(changeUserName)
+    user["name"] = new_name
 
-        connection.commit()
-        cursor.close()
-
-        user["name"] = new_name
-
+#Deletes the user's account from the users table
 def close_account(user, connection):
-    change = input("Are you sure you want to delete you account. This is PERMANENT! (Y/N) ")
-
-    if (change == "n"): return
-
     cursor = connection.cursor()
 
     delete_account = f'DELETE FROM users WHERE id={user["id"]}'
@@ -46,51 +38,63 @@ def close_account(user, connection):
     connection.commit()
     cursor.close()
 
-    print("account deleted...")
-
+#Given an amount the method adds that amount to the user's balance 
+#The method then adds the transaction into the user's account history
 def deposit(connection, user, amount):
     print(user)
     user["money"] += amount
 
     cursor = connection.cursor()
     
+    #Deposit the money
     deposit_user_money = (f'UPDATE users SET money = money+{float(amount)} WHERE id={user["id"]}')
 
     cursor.execute(deposit_user_money)
 
+    #Update the user's account history
     update_history = (f'INSERT INTO bank_history (id, type, amount, balance) VALUES ({user["id"]}, \"{"deposit"}\", {amount}, {user["money"]})')
     cursor.execute(update_history)
 
     connection.commit()
     cursor.close()
-
     
-
+#Given an amount the method subtracts that amount from the user's balance 
+#The method then adds the transaction into the user's account history
 def withdraw(connection, user, amount):
     print(amount)
     user["money"] -= amount
 
     cursor = connection.cursor()
     
+    #Withdraws the money
     withdraw_user_money = (f'UPDATE users SET money = money-{float(amount)} WHERE id={user["id"]}')
 
     cursor.execute(withdraw_user_money)
 
+    #Updates the user's account history
     update_history = (f'INSERT INTO bank_history (id, type, amount, balance) VALUES ({user["id"]}, \"{"withdrawal"}\", {amount}, {user["money"]})')
     cursor.execute(update_history)
 
     connection.commit()
     cursor.close()
 
+#Returns an array of all the transactions the given user has had.
+#The array includes:
+#  - the type of transaction: withdrawal, deposit, transfer
+#  - the amount of money involved in the transaction
+#  - the date and time of the transaction
+#  - the user's balance after the transaction
 def get_history(connection, user):
     cursor = connection.cursor()
 
+    #Get all rows that refer to the given user's history
     get_history = (f'SELECT * FROM bank_history WHERE id={user["id"]}')
 
     cursor.execute(get_history)
 
     history = []
 
+    #For each row add each piece of data
     for row in cursor:
         history.append([row[1].title(), row[2], row[3], row[4]])
     
--- login.py
+++ login.py
@@ -1,5 +1,6 @@
 from random import randint
 
+#Given an account number and pin return the first user that has a matching account number and pin
 def user_login(connection, account_number, pin):
    cursor = connection.cursor()
 
@@ -11,14 +12,17 @@ def user_login(connection, account_number, pin):
 
    response = cursor.fetchone()
 
+   #Convert the user to a dictionary where each column title corresponds to teh user's value in that column
    user = dict((column_names[i], response[i]) for i in range(len(response)))
    return user
 
+#Create a new account given a name and password
 def create_account(connection, name, password):
    account_number = generate_account_number()
 
    cursor = connection.cursor()
 
+   #Add a new account into the users table
    addUser = (f'INSERT INTO users (accountNumber, pin, name, money) VALUES ({account_number}, \"{password}\", \"{name}\", 50)')
 
    cursor.execute(addUser)
@@ -26,8 +30,10 @@ def create_account(connection, name, password):
    connection.commit()
    cursor.close()
 
+   #Return the user by logging him in
    return user_login(connection, account_number, password)
 
+#Generate a random 7-digit number and return it as an account number
 def generate_account_number():
    numb = ""
    for i in range(7):
